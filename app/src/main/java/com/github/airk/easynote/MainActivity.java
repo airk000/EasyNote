de541c271405f4a0490b348e9598875ce521ae99
Kevin Liu <airk908@gmail.com>
2014-10-18 19:49:55 +0800

    First commit
    Signed-off-by: Kevin Liu <airk908@gmail.com>

---
+++ app/src/main/java/com/github/airk/easynote/MainActivity.java
@@ -0,0 +1,51 @@
+package com.github.airk.easynote;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+
+public class MainActivity extends ActionBarActivity {
+    @InjectView(R.id.new_note)
+    Button mNewNote;
+    @InjectView(R.id.recycler_view)
+    RecyclerView mRecycler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.inject(this);
+
+        mRecycler.setLayoutManager(new GridLayoutManager(this, 2));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
